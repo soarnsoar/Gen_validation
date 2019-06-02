94b434dd7bc72fe84c89f70525bd26489238271e
jhchoi <soarnsoar@gmail.com>
2019-06-02 18:56:15 +0900

    Update test_gridpack.sh

--- test_gridpack.sh
+++ test_gridpack.sh
@@ -5,6 +5,10 @@ ARR_TAR=( $(ls *".${EXT}") )
 for gridpack in ${ARR_TAR[@]};do
     echo "@@TEST "${gridpack}"@@"
     TESTDIR=test_${gridpack%".${EXT}"}
+    if [ -f $TESTDIR ];then
+      echo "Already exists ""$TESTDIR"
+      continue
+    fi
     mkdir -p ${TESTDIR}
     pushd $TESTDIR
     tar -xf ../${gridpack}
