4a658ad18607cbaa299bfd0f346aa4f08d37c499
junho choi <soarnsoar@gmail.com>
2019-06-05 21:49:06 +0200

    one by one submit

--- submit.sh
+++ submit.sh
@@ -7,13 +7,13 @@ source /afs/cern.ch/user/j/jhchoi/release_10sec.sh &
 
 
 #mg260
-python run_gridpack_generation.py --branch master --dirname mg260_master --proc dyellell012j_5f_NLO_FXFX
+#python run_gridpack_generation.py --branch master --dirname mg260_master --proc dyellell012j_5f_NLO_FXFX
 #mg261
-python run_gridpack_generation.py --branch mg261 --dirname mg261 --proc dyellell012j_5f_NLO_FXFX
+#python run_gridpack_generation.py --branch mg261 --dirname mg261 --proc dyellell012j_5f_NLO_FXFX
 #mg265
 python run_gridpack_generation.py --branch mg265 --dirname mg265 --proc dyellell012j_5f_NLO_FXFX
 
 
 #source run__master__mg260_master__dyellell012j_5f_NLO_FXFX.sh
-source run__mg261__mg261__dyellell012j_5f_NLO_FXFX.sh
-#source run__mg265__mg265__dyellell012j_5f_NLO_FXFX.sh
+#source run__mg261__mg261__dyellell012j_5f_NLO_FXFX.sh
+source run__mg265__mg265__dyellell012j_5f_NLO_FXFX.sh
