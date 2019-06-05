2330c52a1b2891a829d0065195b515895a5ec24a
junho choi <soarnsoar@gmail.com>
2019-06-05 21:31:59 +0200

    script for single test

---
+++ test_one_gridpack.sh
@@ -0,0 +1,38 @@
+EXT="tar.xz"
+LHENAME='cmsgrid_final.lhe'  
+gridpack=$1
+
+
+echo "@@TEST "${gridpack}"@@"
+TESTDIR=test_${gridpack%".${EXT}"}
+if [ -d $TESTDIR ];then
+    echo "Already exists ""$TESTDIR"
+    continue
+fi
+mkdir -p ${TESTDIR}
+pushd $TESTDIR
+tar -xf ../${gridpack}
+./runcmsgrid.sh 10 10 10 &> test_log.txt
+
+SUCCESS=0
+if [ -f "$LHENAME" ];then
+    
+    #echo "!!"
+    SUCCESS=1
+fi
+
+popd
+   
+
+if [ $SUCCESS -eq 1 ];then
+
+ echo -e "${gridpack}\nDIR=$PWD\n[DONE]\n" | /bin/mailx -s '[DONE]Gridpack ST_TEST ${gridpack}' soarnsoar@gmail.com
+
+else
+
+ echo -e "${gridpack}\nDIR=$PWD\n[FAIL]\n" | /bin/mailx -s '[FAIL]Gridpack ST_TEST ${gridpack}' soarnsoar@gmail.com
+
+
+fi
+    
+
