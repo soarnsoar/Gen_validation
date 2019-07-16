17ded63142b3ff816db1f7f3f71c7f5d88ba7288
your name your last name <soarnsoar@gmail.com>
2019-07-16 19:31:46 +0900

    fix bug on hadd_500 (exclude old file)

--- CMSSW_tools/run_command/hadd_500.py
+++ CMSSW_tools/run_command/hadd_500.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python                                                                                                                                                            \
+#!/usr/bin/env python                                                                                                                                        
+
 import os
 import glob
 import argparse
@@ -17,9 +18,8 @@ else:
 
 
 
-
 os.chdir(directory)
-ROOTFILES=glob.glob('*.root')
+ROOTFILES=glob.glob('HISTO__*__*.root')
 
 
 ngroup=(len(ROOTFILES)//500) +1
@@ -34,12 +34,16 @@ for i_group in range(0,ngroup):
         if i_file%ngroup==i_group:
             this_group_list.append(ROOTFILES[i_file])
 
-
+    #print "[Nfile of a group]=",len(this_group_list)
     os.system('hadd -f temp__'+str(i_group)+'.root '+' '.join(this_group_list) )
     groupfiles.append('temp__'+str(i_group)+'.root')
+    #print 'temp__'+str(i_group)+'.root'
 
 
+#print 'hadd -f histoset.root '+' '.join(groupfiles)    
 os.system('hadd -f histoset.root '+' '.join(groupfiles) )
+#os.system('hadd -f histoset_1.root '+' '.join(groupfiles[1::2]) )
+#os.system('hadd -f histoset_2.root '+' '.join(groupfiles[::2]) )
 
 
 for groupfile in groupfiles:
